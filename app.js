03f08bb73409c027f0520c2da8ae10da13a189d5
JiehongGit <974861604@qq.com>
2017-11-30 00:14:17 +0800

    app.js的修改

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 //加载express模块
 var express = require('express')
+//
+var path = require('path')
 //设置端口(也可以从命令行中设置全局变量)
 //process是一个全局变量，获取环境变量和外围传入的参数
 var port = process.env.PORT || 3000
 //启动web服务器
 var app = express()
 //实例赋给一个变量
-app.set('views','./views')
+app.set('views','./views/pages')
 //设置默认模板引擎
 app.set('view engine','pug')
+//
+//app.use(express.bodyParser())
+//
+app.use(express.static(path.join(__dirname, 'bower_components')))
 //监听端口
 app.listen(port)
 //打印日志
@@ -17,25 +23,85 @@ console.log('serer started on port' + port)
 //路由添加
 app.get('/',function(req,res){
 	res.render('index',{
-		title: 'MovieSite 首页'
+		title: 'MovieSite',
+		movies: [{
+            title: '机械战警',
+            _id: 1,
+            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+		},{
+            title: '机械战警',
+            _id: 2,
+            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+		},{
+            title: '机械战警',
+            _id: 3,
+            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+		},{
+            title: '机械战警',
+            _id: 4,
+            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+		},{
+            title: '机械战警',
+            _id: 5,
+            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+		},{
+            title: '机械战警',
+            _id: 6,
+            poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5'
+		}
+		]
 	})
 })
 
+//detail page
 app.get('/movie/:id',function(req,res){
 	res.render('detail',{
-		title: 'MovieSite 详情页'
+		title: 'MovieSite 详情页',
+		movie: {
+			doctor: '何塞·帕迪里亚',
+			country: '美国',
+			title: '机械战警',
+			year: 2014,
+			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
+			language: '英语',
+			flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
+			summary: '《机械战警》是由何塞·帕迪里亚执导，乔尔·金纳曼、塞缪尔·杰克逊、加里·奥德曼等主演的一部科幻电影，改编自1987年保罗·范霍文执导的同名电影。影片于2014年2月12日在美国上映，2014年2月28日在中国大陆上映。影片的故事背景与原版基本相同，故事设定在2028年的底特律，男主角亚历克斯·墨菲是一名正直的警察，被坏人安装在车上的炸弹炸成重伤，为了救他，OmniCorp公司将他改造成了生化机器人“机器战警”，代表着美国司法的未来。'
+		}
 	})
 })
 
+//admin page
 app.get('/admin/movie',function(req,res){
 	res.render('admin',{
-		title: 'MovieSite 后台录入页'
+		title: 'MovieSite 后台录入页',
+		movie: {
+			title: "",
+			doctor: "",
+			country: "",
+			year: "",
+			poster: "",
+			flash: "",
+			summary: "",
+			language: ""
+		}
 	})
 })
 
+//list page
 app.get('/admin/list',function(req,res){
 	res.render('list',{
-		title: 'MovieSite 列表页'
+		title: 'MovieSite 列表页',
+		movies: [{
+			title: '机械战警',
+			_id: 1,
+			doctor: '何塞·帕迪里亚',
+			country: '美国',
+			year: 2014,
+			poster: 'http://r3.ykimg.com/05160000530EEB63675839160D0B79D5',
+			language: '英语',
+			flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf',
+			summary: '《机械战警》是由何塞·帕迪里亚执导，乔尔·金纳曼、塞缪尔·杰克逊、加里·奥德曼等主演的一部科幻电影，改编自1987年保罗·范霍文执导的同名电影。影片于2014年2月12日在美国上映，2014年2月28日在中国大陆上映。影片的故事背景与原版基本相同，故事设定在2028年的底特律，男主角亚历克斯·墨菲是一名正直的警察，被坏人安装在车上的炸弹炸成重伤，为了救他，OmniCorp公司将他改造成了生化机器人“机器战警”，代表着美国司法的未来。'
+		}]
 	})
 })
 
