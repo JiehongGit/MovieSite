8bb917b26d405174c71ca6772a2dc29acbb62fb4
JiehongGit <974861604@qq.com>
2017-12-05 11:55:11 +0800

    后台录入信息修改中的bug修复

--- public/js/admin.js
+++ public/js/admin.js
@@ -2,10 +2,10 @@
 //通过e可以调出来执行p.click事件时候p元素里面的内容。
 $(function(){
     $('.del').click(function(e){ //e 是被jquery改造后的event对象
-        //return confirm('确定要删除吗');
-        var target = $(e.target); //点的是哪个按钮
-        var id = target.data('id');
-        var tr = $('.item-id-' + id); //拿到表格的一行
+        //return confirm('确定要删除吗')
+        var target = $(e.target) //点的是哪个按钮
+        var id = target.data('id')
+        var tr = $('.item-id-' + id) //拿到表格的一行
 
         $.ajax({
             method: 'DELETE',
@@ -14,9 +14,9 @@ $(function(){
             .done(function(results){
                 if(results.success === 1){
                     if(tr.length >0 ){
-                        tr.remove();
+                        tr.remove()
                     }
                 }
-            });
-    });
-});
+            })
+    })
+})
